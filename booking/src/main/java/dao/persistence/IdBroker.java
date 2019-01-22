79d8b8d5f5ee10cc2e178ea542f49e4b8ccdc999
Utente <Utente@DESKTOP-6G4O6P0>
2019-01-22 22:52:14 +0100

    ciao

--- booking/src/main/java/dao/persistence/IdBroker.java
+++ booking/src/main/java/dao/persistence/IdBroker.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+                                              
 public class IdBroker {
 
 	private static final String query = "SELECT ffzg_zs_nextval(?) AS id";
